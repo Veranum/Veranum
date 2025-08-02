0719a60e86b9fde1635e43cfd26bc224c96a0e43
DavNat13 <natdavn@gmail.com>
2025-08-02 13:35:49 -0400

    Corrección Variable de Validación en RegisterPage

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -17,7 +17,7 @@ const validateRun = (run) => {
 
 const validateEmail = (email) => {
   if (!email) return 'El correo electrónico es obligatorio.';
-  const emailRegex = /^[\w.-]+@[\w.-]+\.[a-zA-Z]{2,6}$/;
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
   if (!emailRegex.test(email)) return 'El formato del correo electrónico no es válido.';
   return '';
 };
